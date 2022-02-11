ee499862542af413fcde74874bcea7f13b1c975f
Gerard Hynes <gkhynes@gmail.com>
2022-02-11 21:30:55 +0000

    Add low-level API example with JSON

---
+++ src/main/scala/part2_lowlevelserver/LowLevelRest.scala
@@ -0,0 +1,133 @@
+package part2_lowlevelserver
+
+import akka.pattern.ask
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.Uri.Query
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import part2_lowlevelserver.GuitarDB.{CreateGuitar, FindAllGuitars, GuitarCreated}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+// Step 1
+import spray.json._
+
+case class Guitar(make: String, model: String)
+
+object GuitarDB {
+  case class CreateGuitar(guitar: Guitar)
+  case class GuitarCreated(id: Int)
+  case class FindGuitar(id: Int)
+  case object FindAllGuitars
+}
+
+class GuitarDB extends Actor with ActorLogging {
+  import GuitarDB._
+
+  var guitars: Map[Int, Guitar] = Map()
+  var currentGuitarId: Int = 0
+
+  override def receive: Receive = {
+    case FindAllGuitars =>
+      log.info("Searching for all guitars")
+      sender() ! guitars.values.toList
+    case FindGuitar(id) =>
+      log.info(s"Searching guitar by id: $id")
+      sender() ! guitars.get(id)
+
+    case CreateGuitar(guitar) =>
+      log.info(s"Adding guitar $guitar with id $currentGuitarId")
+      guitars = guitars + (currentGuitarId -> guitar)
+      sender() ! GuitarCreated(currentGuitarId)
+      currentGuitarId += 1
+  }
+}
+
+// Step 2
+trait GuitarStoreJsonProtocol extends DefaultJsonProtocol {
+  // Step 3
+  // formats class with 2 parameters
+  implicit val guitarFormat = jsonFormat2(Guitar)
+}
+
+object LowLevelRest extends App with GuitarStoreJsonProtocol {
+  implicit val system = ActorSystem("LowLevelRest")
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  /*
+    GET on localhost:8080/api/guitar => All the guitars in the store
+    POST on localhost:8080/api/guitar => Insert new guitar into the store
+  */
+
+  // JSON -> marshalling
+  val simpleGuitar = Guitar("Fender", "Stratocaster")
+  // converts JSON object to ast and then string
+  println(simpleGuitar.toJson.prettyPrint)
+
+  // unmarshalling
+  val simpleGuitarJsonString =
+  """
+    |{
+    |  "make": "Fender",
+    |  "model": "Stratocaster"
+    |}
+  """.stripMargin
+  println(simpleGuitarJsonString.parseJson.convertTo[Guitar])
+
+  /*
+   setup
+  */
+  val guitarDb = system.actorOf(Props[GuitarDB], "LowLevelGuitarDB")
+
+  val guitarList = List(
+    Guitar("Fender", "Stratocaster"),
+    Guitar("Gibson", "Les Paul"),
+    Guitar("Martin", "LX1")
+  )
+
+  guitarList.foreach { guitar =>
+    guitarDb ! CreateGuitar(guitar)
+  }
+
+  /*
+  server code
+  */
+  implicit val defaultTimeout = Timeout(2 seconds)
+
+  val requestHandler: HttpRequest => Future[HttpResponse] = {
+    case HttpRequest(HttpMethods.GET, Uri.Path("/api/guitar"),_,_,_) =>
+      val guitarsFuture: Future[List[Guitar]] = (guitarDb ? FindAllGuitars).mapTo[List[Guitar]]
+      guitarsFuture.map { guitars =>
+        HttpResponse(
+          entity = HttpEntity(
+            ContentTypes.`application/json`,
+            guitars.toJson.prettyPrint
+          )
+        )
+      }
+
+    case HttpRequest(HttpMethods.POST, Uri.Path("/api/guitar"), _, entity, _) =>
+      // entities are a Source[ByteString]
+      val strictEntityFuture = entity.toStrict(3 seconds)
+      strictEntityFuture.flatMap { strictEntity =>
+        val guitarJsonString = strictEntity.data.utf8String
+        val guitar = guitarJsonString.parseJson.convertTo[Guitar]
+        val guitarCreatedFuture: Future[GuitarCreated] = (guitarDb ? CreateGuitar(guitar)).mapTo[GuitarCreated]
+        guitarCreatedFuture.map { guitarCreated =>
+          HttpResponse(StatusCodes.OK)
+        }
+      }
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      Future {
+        HttpResponse(status = StatusCodes.NotFound)
+      }
+  }
+
+  Http().bindAndHandleAsync(requestHandler, "localhost", 8080)
+}
